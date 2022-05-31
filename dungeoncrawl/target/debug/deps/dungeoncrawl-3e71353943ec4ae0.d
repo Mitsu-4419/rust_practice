3e5b2d4793a459d8f31cfab752f04c6a38077004
Mitsutomo Yamada <Mitsu44@Mac-Studio.local>
2022-05-31 19:49:05 +0900

    make map_builder trait

--- dungeoncrawl/target/debug/deps/dungeoncrawl-3e71353943ec4ae0.d
+++ dungeoncrawl/target/debug/deps/dungeoncrawl-3e71353943ec4ae0.d
@@ -1,10 +1,11 @@
-/Users/Mitsu44/Documents/rust_practice/dungeoncrawl/target/debug/deps/dungeoncrawl-3e71353943ec4ae0: src/main.rs src/map.rs src/map_builder.rs src/camera.rs src/components.rs src/spawner.rs src/systems/mod.rs src/systems/map_render.rs src/systems/player_input.rs src/systems/entity_render.rs src/systems/random_move.rs src/systems/end_turns.rs src/systems/movement.rs src/systems/hud.rs src/systems/tooltips.rs src/systems/combat.rs src/systems/chasing.rs src/systems/fov.rs src/turn_state.rs
+/Users/Mitsu44/Documents/rust_practice/dungeoncrawl/target/debug/deps/dungeoncrawl-3e71353943ec4ae0: src/main.rs src/map.rs src/map_builder/mod.rs src/map_builder/empty.rs src/camera.rs src/components.rs src/spawner.rs src/systems/mod.rs src/systems/map_render.rs src/systems/player_input.rs src/systems/entity_render.rs src/systems/random_move.rs src/systems/end_turns.rs src/systems/movement.rs src/systems/hud.rs src/systems/tooltips.rs src/systems/combat.rs src/systems/chasing.rs src/systems/fov.rs src/turn_state.rs
 
-/Users/Mitsu44/Documents/rust_practice/dungeoncrawl/target/debug/deps/dungeoncrawl-3e71353943ec4ae0.d: src/main.rs src/map.rs src/map_builder.rs src/camera.rs src/components.rs src/spawner.rs src/systems/mod.rs src/systems/map_render.rs src/systems/player_input.rs src/systems/entity_render.rs src/systems/random_move.rs src/systems/end_turns.rs src/systems/movement.rs src/systems/hud.rs src/systems/tooltips.rs src/systems/combat.rs src/systems/chasing.rs src/systems/fov.rs src/turn_state.rs
+/Users/Mitsu44/Documents/rust_practice/dungeoncrawl/target/debug/deps/dungeoncrawl-3e71353943ec4ae0.d: src/main.rs src/map.rs src/map_builder/mod.rs src/map_builder/empty.rs src/camera.rs src/components.rs src/spawner.rs src/systems/mod.rs src/systems/map_render.rs src/systems/player_input.rs src/systems/entity_render.rs src/systems/random_move.rs src/systems/end_turns.rs src/systems/movement.rs src/systems/hud.rs src/systems/tooltips.rs src/systems/combat.rs src/systems/chasing.rs src/systems/fov.rs src/turn_state.rs
 
 src/main.rs:
 src/map.rs:
-src/map_builder.rs:
+src/map_builder/mod.rs:
+src/map_builder/empty.rs:
 src/camera.rs:
 src/components.rs:
 src/spawner.rs:
